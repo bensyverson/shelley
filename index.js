199aba601ca95b2017cd34b3dce64bd48c200611
Ben Syverson <ben@bensyverson.com>
2018-12-27 15:15:20 -0600

    Refactoring and cleaning up

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-'use strict';
+'use strict'
 
-var Shelley = require('./lib/Shelley.js');
-var sh = new Shelley();
+const path = process.argv[2]
 
-sh.build();
+const Shelley = require('./lib/Shelley.js')
+const sh = new Shelley()
+
+try {
+	sh.build()
+} catch (e) {
+	console.log(e)
+}
